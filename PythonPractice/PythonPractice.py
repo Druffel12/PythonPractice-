6475ede144b854f57bfb4b16493fc98b935e769d
Druffel12 <Druffel12@gmail.com>
2019-06-05 12:29:18 -0700

    First commit

---
+++ PythonPractice/PythonPractice.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+print ("dickbutt")
+
+#Comment
+
+name = "Cole"
+
+name = 15
+
+print(name)
+
+# math types
+# +,-,*,/,%,**,//
+
+print("5 + 2 =", 5+2)
+print("5 - 2 =", 5-2)
+print("5 * 2 =", 5*2)
+print("5 / 2 =", 5/2)
+print("5 % 2 =", 5%2)
+print("5 ** 2 =", 5**2)
+print("5 // 2 =", 5//2)
+
+#Pemdas matters
+
+quote = "\" Always 
+
+
