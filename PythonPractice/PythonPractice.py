8d415823861073719ad70734bef232dc7a2b7de4
Druffel12 <Druffel12@gmail.com>
2019-06-05 17:48:58 -0700

    Exercise 1 and 2 done

--- PythonPractice/PythonPractice.py
+++ PythonPractice/PythonPractice.py
@@ -22,8 +22,40 @@ print("5 % 2 =", 5%2)
 print("5 ** 2 =", 5**2)
 print("5 // 2 =", 5//2)
 
-#Pemdas matters
+# Pemdas matters
 
-quote = "\" Always 
+#
+#
+#
+
+print ("practices")
+
+# 1.
+# Printing to the console
+print ("Hello World!")
+print ("Hello Again!")
+print ("I like typing this")
+print ("This is fun")
+print ('Yay! Printing')
+print ("I'd much rather you 'not' ")
+print ('I "said" do not touch this')
+print ("Python is pretty fun so far!")
+
+# 2.
+# Commenting 
+# A comment, this is so you can read and document your code
+# Anything after the # is ignored by python
+
+print ("I could have code like this") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print ("This will run")
+
+# 3.
+# Numbers and Math
+# pg 20
+
 
 
